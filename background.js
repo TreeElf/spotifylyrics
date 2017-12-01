53512a828cd4b7ec55c07ee284e9a01cebd63170
TreeElf <32620629+TreeElf@users.noreply.github.com>
2017-12-01 16:08:55 -0600

    updated logic

--- background.js
+++ background.js
@@ -1,3 +1,43 @@
-var counter;
-
+var id;
+var on = false;
 chrome.browserAction.setIcon({ path: "icongray38.png" });
+function checkForSpotify(a, b, c) {
+	if(!on) {
+		if(b.url!=null) {
+			//console.log(b.url);
+			//console.log(c);
+			if(b.url.includes("open.spotify.com")) {
+				chrome.browserAction.setIcon({ path: "icon38.png" });
+				id = a;
+			}
+		}
+	}
+}
+
+
+function checkSong(a,b,c) {
+
+}
+
+function checkAllTabs(a){
+	var ts = a;
+	for(i = 0;i<ts.length;i++) {
+		if(ts[i].url.includes("open.spotify.com")) {
+			chrome.browserAction.setIcon({ path: "icon38.png" });
+			id = a;
+			on = true;
+		}
+	}
+}
+function checkSpotifyClose(a,b){
+	if(a==id) {
+		id=null;
+		on=false;
+		chrome.browserAction.setIcon({ path: "icongray38.png"});
+	}
+}
+
+
+chrome.tabs.query({}, checkAllTabs);
+chrome.tabs.onUpdated.addListener(checkForSpotify);
+chrome.tabs.onRemoved.addListener(checkSpotifyClose);
